21c76faa45adf7b5954619fefc42a967a8fb65c9
CheaVarin12 <cheavarin7@gmail.com>
2026-05-04 22:18:42 +0700

    fix

--- app/Http/Resources/Web/ListOfValue/OurStoryResource.php
+++ app/Http/Resources/Web/ListOfValue/OurStoryResource.php
@@ -15,24 +15,24 @@ class OurStoryResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id,
-            'page' => $this->page,
-            'title' => $this->title,
-            'content' => [
-                'en'=>$this->content['en'],
-                'km'=>$this->content['km']
+            'id'        => $this->id,
+            'page'      => $this->page,
+            'title'     => $this->title,
+            'image'     => $this->image,
+            'image_url' => $this->resolveAssetUrl($this->image),
+            'content'   => [
+                'en' => $this->content['en'] ?? null,
+                'km' => $this->content['km'] ?? null,
             ],
             'dataDetail' => collect($this->content['dataDetail'] ?? [])
                 ->sortBy('ordering')
                 ->map(function ($item) {
                     return [
-                        'icon' => $item['icon'] ?? null,
-                        'icon_url' => isset($item['icon'])
-                            ? $item['icon']
-                            : asset('images/no.jpg'),
-                        'ordering' => $item['ordering'] ?? null,
-                        'title_en' => $item['title_en'] ?? null,
-                        'title_km' => $item['title_km'] ?? null,
+                        'icon'           => $item['icon'] ?? null,
+                        'icon_url'       => $this->resolveAssetUrl($item['icon'] ?? null),
+                        'ordering'       => $item['ordering'] ?? null,
+                        'title_en'       => $item['title_en'] ?? null,
+                        'title_km'       => $item['title_km'] ?? null,
                         'description_en' => $item['description_en'] ?? null,
                         'description_km' => $item['description_km'] ?? null,
                     ];
@@ -40,5 +40,18 @@ class OurStoryResource extends JsonResource
                 ->values()
                 ->toArray(),
         ];
+    }
+
+    private function resolveAssetUrl(?string $file): ?string
+    {
+        if (!$file) {
+            return null;
+        }
+
+        if (filter_var($file, FILTER_VALIDATE_URL)) {
+            return $file;
+        }
+
+        return asset('storage/list-of-value/' . $file);
     }
 }
