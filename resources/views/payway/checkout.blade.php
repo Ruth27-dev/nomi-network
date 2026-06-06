78d1d100cfaac5cb156d45ad3145ac2be0055b8e
Naruth <sopharuth.club@gmail.com>
2026-06-06 23:47:23 +0700

    add aba testing

--- resources/views/payway/checkout.blade.php
+++ resources/views/payway/checkout.blade.php
@@ -8,10 +8,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 </head>
 <body>
-    @php
-        $paymentOption = $params['payment_option'] ?? 'abapay_khqr';
-    @endphp
-
     <div id="aba_main_modal" class="aba-modal">
         <div class="aba-modal-content">
             <form id="aba_merchant_request" target="aba_webservice" action="{{ $params['api_url'] }}" method="POST">
@@ -24,36 +20,18 @@
                 <input type="hidden" name="phone" value="{{ $params['phone'] }}">
                 <input type="hidden" name="req_time" value="{{ $params['req_time'] }}">
                 <input type="hidden" name="merchant_id" value="{{ $params['merchant_id'] }}">
+                <input type="hidden" name="payment_option" value="{{ $params['payment_option'] ?? 'abapay_khqr' }}">
                 <input type="hidden" name="view_type" value="{{ $params['view_type'] ?? 'popup' }}">
             </form>
         </div>
     </div>
 
-    <div class="container" style="margin-top: 75px;margin: 0 auto;">
-        <div style="width: 240px;margin: 0 auto;">
-            <div class="wpr_payment_option">
-                <div style="margin-top: 10px">
-                    <input type="radio" name="payment_option" class="payment_option" style="margin: 14px;float: left;" checked value="{{ $paymentOption }}">
-                    <label class="paymentOption" for="khqr">
-                        <span class="detailCard002" style="margin-left: 16px; float: right; position: absolute;">
-                            <strong><span class="titleCard">ABA PayWay</span><br></strong>
-                            <span class="detailCard003" style="margin-top: 5px;">Complete your payment</span>
-                        </span>
-                    </label>
-                </div>
-            </div>
-            <h2>TOTAL: {{ $params['amount'] }}</h2>
-            <input type="button" id="checkout_button" value="Checkout Now">
-        </div>
-    </div>
+    <div class="container" style="height: 80vh;"></div>
 
     <script src="https://checkout.payway.com.kh/plugins/checkout2-0.js"></script>
     <script>
         $(document).ready(function(){
-            $('#checkout_button').click(function(){
-                $('#aba_merchant_request').append($('.payment_option:checked'));
-                AbaPayway.checkout();
-            });
+            AbaPayway.checkout();
         });
     </script>
 </body>
