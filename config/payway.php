38651e44be0c548f70e5b1602e44031c067bedf6
Naruth <sopharuth.club@gmail.com>
2026-06-06 15:32:01 +0700

    add aba testing

--- config/payway.php
+++ config/payway.php
@@ -11,9 +11,9 @@ return [
     |   https://checkout.payway.com.kh/api/payment-gateway/v1/payments/purchase
     */
 
-    'merchant_id'   => env('PAYWAY_MERCHANT_ID', ''),
-    'api_key'       => env('PAYWAY_API_KEY', ''),
-    'api_url'       => env('PAYWAY_API_URL', 'https://checkout-sandbox.payway.com.kh/api/payment-gateway/v1/payments/purchase'),
+    'merchant_id'   => env('PAYWAY_MERCHANT_ID', env('ABA_PAYWAY_MERCHANT_ID', '')),
+    'api_key'       => env('PAYWAY_API_KEY', env('ABA_PAYWAY_API_KEY', '')),
+    'api_url'       => env('PAYWAY_API_URL', env('ABA_PAYWAY_API_URL', 'https://checkout-sandbox.payway.com.kh/api/payment-gateway/v1/payments/purchase')),
     'checkout_url'  => env('PAYWAY_CHECKOUT_URL', 'https://checkout-sandbox.payway.com.kh'),
 
 ];
